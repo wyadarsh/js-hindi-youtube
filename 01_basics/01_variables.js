26da184fda8fe237dee4e1d66574c1bfa10256c5
wyadarsh <148446932+wyadarsh@users.noreply.github.com>
2025-01-10 06:55:35 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=11111
+let accountEmail="adarsh@google.com"
+var accountPass="12345"
+accountCity="jaipur"
+let accountState;
+//accountId=2//not allowed
+accountEmail="si@gmail.com"
+accountPass="66"
+accountCity="kolkata"//do not use this format to declare a varaiable
+//console.log(accountId);
+//do not use var for variable declararation use let keyword;
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+
+
+//alert("hello")//we are using nodejs not browser
+
+let name="hitesh"
+let age=25
+let isLoggedIn=false;
+
+//data tyepes:
+
+//number
+//bigint
+//string=write in single inverted commas or double
+//boolean=true or false
+
+//null=standlonevalue
+//undefined
+//symbol=unique
+
+
+//object
+console.log(typeof "hitesh")
+console.log(typeof age )
+console.log(typeof null );//object
+console.log(typeof undefined);//undefined
