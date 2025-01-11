e4819bd432c225141e52fd55df698d72871ab61b
wyadarsh <148446932+wyadarsh@users.noreply.github.com>
2025-01-11 21:35:30 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+let score1=33// gives number
+let score2="hitesh"//true//undefined//null//"34abc"// gives string bcoz the number is written in inverted comma
+
+console.log(typeof score1)
+console.log(typeof score2)
+
+let valueInNumber=Number(score2) // to convert string score2 in number we use capital Number
+console.log(typeof valueInNumber)
+console.log(valueInNumber)//NaN not a number
+
+let isLoggedIn=1
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+
+//*********Operations *********
+let value=3
+let negValue=-value
+console.log(negValue)
+
+//complex:
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+
+
+console.log(+true);
+console.log(+"");
+
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+//learn prefix and postfix from mdn
+
